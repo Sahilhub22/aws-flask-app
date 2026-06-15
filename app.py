6f91163bf55c8a372f319e1d91c8db459076c6e4
Sahilhub22 <shahil12345q@gmail.com>
2026-06-15 23:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+import pymysql
 
 app = Flask(__name__)
 
+db = pymysql.connect(
+    host="flask-db.cbummsam8br0.us-east-2.rds.amazonaws.com",
+    user="admin",
+    password="Sahilpoiu",
+    database="mydb"
+)
+
 @app.route('/')
 def home():
-    return "AWS HA Flask Application Running"
+    return '''
+    <form action="/save" method="post">
+        <input name="username">
+        <button type="submit">Save</button>
+    </form>
+    '''
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/save', methods=['POST'])
+def save():
+    username = request.form['username']
+
+    cursor = db.cursor()
+    cursor.execute(
+        "INSERT INTO users(name) VALUES(%s)",
+        (username,)
+    )
+
+    db.commit()
+
+    return "Saved Successfully"
+
+app.run(host='0.0.0.0', port=5000)
